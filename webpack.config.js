82623e05dc41f99788d51e8a671f7edb92a4701f
Alex Eliseev <eajorianp@gmail.com>
2019-09-29 20:03:22 +0300

    Add fonts to project

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
             },
             {
                 test: /\.(eot|ttf|woff|woff2)$/,
-                loader: 'file-loader?name=./vendor/fonts/[name].[ext]'
+                loader: 'file-loader?name=../vendor/fonts/[name].[ext]'
             }
         ]
     },
